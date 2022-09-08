3d8a058f8eb778820c45fc32fb6d78b696099b86
mira-kine <mirak0419@gmail.com>
2022-09-07 21:00:00 -0700

    added preventing default in searchForm to fix clearning submission bug

---
+++ src/components/ImageDetailsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function ImageDetailsPage() {
+	// TODO: new view when user clicks a result.
+	//  this page will render a back button that returns the user to the list view
+	// without React Router (conditional rendering)
+	// and has the artwork whose title the user just clicke don
+	return <div>ImageDetailsPage</div>;
+}
