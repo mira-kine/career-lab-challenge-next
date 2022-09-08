63c666c036e2493d150ca57c6c346aed79dc0548
mira-kine <mirak0419@gmail.com>
2022-09-07 21:09:45 -0700

    rendering list in app with piece and artist without styling

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,11 @@ import './App.css';
 import { searchArtworks } from '../utils/api';
 import { SearchForm } from './SearchForm';
 import { Footer } from './Footer';
+import { useState } from 'react';
 
 export function App() {
+	const [artwork, setArtwork] = useState();
+
 	function onSearchSubmit(query) {
 		// Search for the users's query.
 		// TODO: render the results, instead of logging them to the console.
@@ -13,14 +16,21 @@ export function App() {
 		// our UI, we need to make real requests!
 		// @see: ./src/uitls/api.js
 		searchArtworks(query).then((json) => {
-			console.log(json);
+			setArtwork(json.data);
 		});
 	}
-
+	console.log('artwork', artwork);
 	return (
 		<div className="App">
-			<h1>TCL Career Lab Art Finder</h1>
 			<SearchForm onSearchSubmit={onSearchSubmit} />
+			<h1>TCL Career Lab Art Finder</h1>
+			{artwork &&
+				artwork.map((art) => (
+					<>
+						<h3 key={art.id}>{art.title}</h3>
+						<h4>{art.artist_title}</h4>
+					</>
+				))}
 			<Footer />
 		</div>
 	);
